217e5cd43d6c01d660bc0d0f6c27fdbeb00a7b20
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-02 23:07:41 +0200

    feat: add CommunityService and CommunityServiceImpl

---
+++ core/src/main/java/com/dam/commune/service/CommunityService.java
@@ -0,0 +1,16 @@
+package com.dam.commune.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.dam.commune.entity.Community;
+
+public interface CommunityService {
+    List<Community> findAll();
+
+    Optional<Community> findById(Long id);
+
+    Community save(Community community);
+    
+    void deleteById(Long id);
+}
---
+++ core/src/main/java/com/dam/commune/service/CommunityServiceImpl.java
@@ -0,0 +1,45 @@
+package com.dam.commune.service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+import org.springframework.stereotype.Service;
+
+import com.dam.commune.entity.Community;
+import com.dam.commune.repository.CommunityRepository;
+
+@Service
+public class CommunityServiceImpl implements CommunityService {
+
+    private final CommunityRepository communityRepository;
+
+    public CommunityServiceImpl(CommunityRepository communityRepository) {
+        this.communityRepository = communityRepository;
+    }
+
+    @Override
+    public List<Community> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    @Override
+    public Optional<Community> findById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findById'");
+    }
+
+    @Override
+    public Community save(Community community) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'save'");
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteById'");
+    }
+    
+}
