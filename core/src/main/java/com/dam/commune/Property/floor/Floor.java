acccb1694383502d89d064c6420f2d379c11bb3c
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-13 20:43:37 +0200

    feat: add Getter and SetterLombok annotations for Floor class

--- core/src/main/java/com/dam/commune/Property/floor/Floor.java
+++ core/src/main/java/com/dam/commune/Property/floor/Floor.java
@@ -3,8 +3,12 @@ package com.dam.commune.property.floor;
 import com.dam.commune.property.Property;
 
 import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class Floor extends Property {
     private Integer floorNumber;
     private String letter;
