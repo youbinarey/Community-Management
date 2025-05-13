f3e695debacca968d286a11f2dcbc63901b7c46c
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-13 20:21:19 +0200

    fix: update StorageRoom class to extend Property

--- core/src/main/java/com/dam/commune/Property/StorageRoom/StorageRoom.java
+++ core/src/main/java/com/dam/commune/Property/StorageRoom/StorageRoom.java
@@ -1,8 +1,10 @@
 package com.dam.commune.property.StorageRoom;
 
+import com.dam.commune.property.Property;
+
 import jakarta.persistence.Entity;
 
 @Entity
-public class StorageRoom {
+public class StorageRoom extends Property{
   private int storageNumber;
 }
