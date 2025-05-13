c55efecee071411b65681d94fa23fc1cc8ab8f5f
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-13 18:14:55 +0200

    feat: add Property entity

---
+++ core/src/main/java/com/dam/commune/Property/Property.java
@@ -0,0 +1,34 @@
+package com.dam.commune.property;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Property {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String referenciaCatastral;
+
+    @Column(nullable = false)
+    private Double metros2;
+
+    // La relación con Community 
+    // @ManyToOne
+    // private Community community;
+}
