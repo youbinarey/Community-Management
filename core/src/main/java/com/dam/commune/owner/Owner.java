e7337cec611d9f6829a05f6b16028c65d708cf75
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-15 21:07:24 +0200

    refactor: reorganize Owner entity structure and ensure proper relationship mapping with BankAccount and Property

--- core/src/main/java/com/dam/commune/owner/Owner.java
+++ core/src/main/java/com/dam/commune/owner/Owner.java
@@ -1,18 +1,22 @@
 package com.dam.commune.owner;
 
-
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.dam.commune.bankAccount.BankAccount;
 import com.dam.commune.property.Property;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,11 +26,12 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Owner {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
-    
+
     @Column(nullable = false, unique = true)
     private String dni;
     @Column(nullable = false)
@@ -40,10 +45,14 @@ public class Owner {
     @Column(nullable = false)
     LocalDate birthDate;
 
-    @OneToMany(mappedBy = "owner")
-    private List<Property> properties;
+   
 
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "bank_account_id", unique = true)
+    private BankAccount bankAccount;
+
+    @Builder.Default
     @OneToMany(mappedBy = "owner")
-    private List<BankAccount> bankAccounts;
+    private List<Property> properties = new ArrayList<>();
 
 }
