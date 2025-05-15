9bdce66fff279947205db43a2c4b4dca65ff9034
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-15 21:08:00 +0200

    refactor: enhance Floor entity with annotations for better structure and builder support

--- core/src/main/java/com/dam/commune/Property/floor/Floor.java
+++ core/src/main/java/com/dam/commune/Property/floor/Floor.java
@@ -3,14 +3,18 @@ package com.dam.commune.property.floor;
 import com.dam.commune.property.Property;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Setter
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
+@SuperBuilder
 public class Floor extends Property {
     private Integer floorNumber;
     private String letter;
