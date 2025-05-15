c99c057c1bab4af15ed6208dfeade3b93c0ea8bc
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-15 21:11:56 +0200

    refactor: update Garage to Parking entity and add buillder annotations

--- core/src/main/java/com/dam/commune/Property/Garage/Parking.java
+++ core/src/main/java/com/dam/commune/Property/Garage/Parking.java
@@ -3,12 +3,18 @@ package com.dam.commune.property.Garage;
 import com.dam.commune.property.Property;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
+@Setter
 @Getter
+@AllArgsConstructor
 @NoArgsConstructor
-public class Garage extends Property{
-    private int parkingSpots;
+@SuperBuilder
+public class Parking extends Property{
+    private int num;
 }
