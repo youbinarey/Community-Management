2737fd6e013e74a8ee014c921288ef5b9db51468
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-23 17:55:45 +0200

    feat: create CommunityService for managing community

---
+++ frontend/src/app/service/community.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Community } from '../models/community';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+  export class CommunityService {
+    private apiUrl = 'http://localhost:8080/api/commune/communities/dto';
+
+    communities: Community[];
+  constructor(private http: HttpClient) {
+      this.communities = [];
+   }
+
+  getCommunities(): Observable<Community[]> {
+    return this.http.get<Community[]>(this.apiUrl);
+  }
+
+  getCommunityById(id: number): Observable<Community> {
+    return this.http.get<Community>(`${this.apiUrl}/${id}/detail`);
+  }
+}
+
+
