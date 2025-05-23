89698bcfc4898456daf0ee10447a3f84a700de84
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-23 17:56:10 +0200

    feat: update routing configuration and add HTTP client support

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { CommunitiesComponent } from './pages/communities/communities.component';
 
-export const routes: Routes = [];
+
+
+export const routes: Routes = [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'communities', component: CommunitiesComponent}
+
+];
