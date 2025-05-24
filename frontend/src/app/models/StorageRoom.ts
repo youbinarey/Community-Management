1beb8fd9d1076fecdfcb16a3d155f39e13bc88df
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-24 21:35:58 +0200

    feat: add Parking and StorageRoom interfaces for community data modeling

---
+++ frontend/src/app/models/StorageRoom.ts
@@ -0,0 +1,8 @@
+export interface StorageRoom{
+    id: number;
+    cadastralReference: string;
+    squareMeters: number;
+    storageNumber: number;
+    communityName: string;
+    ownerName: string;
+}
