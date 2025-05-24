18f55dce31786a3abf8f1ada923167b7bd19d07f
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-24 13:00:38 +0200

    feat: add CORS support to OwnerController

--- core/src/main/java/com/dam/commune/owner/OwnerController.java
+++ core/src/main/java/com/dam/commune/owner/OwnerController.java
@@ -3,6 +3,7 @@ package com.dam.commune.owner;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/owner")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class OwnerController {
     private final OwnerService ownerService;
     
