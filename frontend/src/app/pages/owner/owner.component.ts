a400e6b498694486b1a9ab2d55220fcb7b721dff
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-24 13:24:57 +0200

    feat: implement OwnerComponent

---
+++ frontend/src/app/pages/owner/owner.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Owner } from '../../models/Owner';
+import { OwnerService } from '../../service/owner.service';
+import { NgFor, NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-owner',
+  imports: [NgIf, NgFor],
+  templateUrl: './owner.component.html',
+  styleUrl: './owner.component.scss'
+})
+export class OwnerComponent implements OnInit {
+viewOwnerDetails(_t12: Owner) {
+throw new Error('Method not implemented.');
+}
+  owners: Owner[] = [];
+
+  constructor(private ownerService: OwnerService) { }
+
+  ngOnInit(): void {
+    this.ownerService.getAllOwners().subscribe((data) =>{
+      this.owners = data;
+    });   
+  }
+
+  // Additional methods and properties can be added as needed
+
+}
