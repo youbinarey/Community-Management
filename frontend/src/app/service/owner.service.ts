3dbdb831f2cdda342b6c51321ceca9c066a7675f
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-24 13:24:41 +0200

    feat: create OwnerService

---
+++ frontend/src/app/service/owner.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Owner } from '../models/Owner';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OwnerService {
+
+  private apiUrl = 'http://localhost:8080/api/commune/owner';
+
+  constructor(private http: HttpClient) { }
+
+  getAllOwners(): Observable<Owner[]> {
+    return this.http.get<Owner[]>(`${this.apiUrl}/dto`);
+  }
+    
+   
+}
