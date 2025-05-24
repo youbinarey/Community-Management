2147fddfbc4bdfee4223437b9847765f778bad8a
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-24 13:25:07 +0200

    feat: add route for OwnerComponent

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CommunitiesComponent } from './pages/communities/communities.component';
 import { FlatComponent } from './pages/properties/flat/flat.component';
+import { OwnerComponent } from './pages/owner/owner.component';
 
 
 
 export const routes: Routes = [
     {path: 'dashboard', component: DashboardComponent},
     {path: 'communities', component: CommunitiesComponent},
-    {path: 'properties/flat/:communityId', component: FlatComponent}
+    {path: 'properties/flat/:communityId', component: FlatComponent},
+    {path: 'owners', component: OwnerComponent}
+
 ];
