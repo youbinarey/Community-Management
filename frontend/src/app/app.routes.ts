838da80e5619034cbd5c83111d23e2b09e6b1801
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-24 21:37:10 +0200

    fix: fix routes for Storageroom and Parking components

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { CommunitiesComponent } from './pages/communities/communities.component';
 import { FlatComponent } from './pages/properties/flat/flat.component';
 import { OwnerComponent } from './pages/owner/owner.component';
+import { StorageroomComponent } from './properties/storageroom/storageroom.component';
+import { ParkingComponent } from './properties/parking/parking.component';
 
 
 
@@ -10,6 +12,8 @@ export const routes: Routes = [
     {path: 'dashboard', component: DashboardComponent},
     {path: 'communities', component: CommunitiesComponent},
     {path: 'properties/flat/:communityId', component: FlatComponent},
+    {path: 'properties/storageroom/:communityId', component: StorageroomComponent},
+    {path: 'properties/parking/:communityId', component: ParkingComponent},
     {path: 'owners', component: OwnerComponent}
 
 ];
