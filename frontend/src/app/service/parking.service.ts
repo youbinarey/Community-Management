53e65f14bbc619a78e01dcf10bc59cd7d6546331
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-24 21:35:26 +0200

    feat: add ParkingService to retrieve parking data by community ID

---
+++ frontend/src/app/service/parking.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Parking } from '../models/Parking';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ParkingService {
+
+  private apiUrl = 'http://localhost:8080/api/commune'; 
+
+  constructor(private http: HttpClient) { }
+
+  getParkingsByCommunity(communityId: number) : Observable<Parking[]> {
+    return this.http.get<Parking[]>(`${this.apiUrl}/properties/parking/community/${communityId}`);
+  }
+}
