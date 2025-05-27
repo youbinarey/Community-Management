ee6694ea6dc642623802e9a10f9eca082d57c455
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-27 20:09:32 +0200

    feat: add InvoicePdfDTO and InvoicePdfMapper for PDF generation

---
+++ core/src/main/java/com/dam/commune/invoice/InvoicePdfDTO.java
@@ -0,0 +1,22 @@
+package com.dam.commune.invoice;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InvoicePdfDTO {
+    private String address;
+    private String date;      
+    private Double electricity;
+    private Double water;
+    private Double trash;
+    private Double elevator;
+    private Double maintenance;
+}
