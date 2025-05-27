2c89d32470d2a7c823bd35d4eb7a5f43e56514c0
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-27 18:26:37 +0200

    feat: add InvoiceDTO, InvoiceMapper, and InvoiceRepository for invoice management

---
+++ core/src/main/java/com/dam/commune/invoice/InvoiceDTO.java
@@ -0,0 +1,21 @@
+package com.dam.commune.invoice;
+
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InvoiceDTO {
+    private Long id;
+    private LocalDate date;
+    private Double electricity;
+    private Double water;
+    private Double trash;
+    private Double elevator;
+    private Double maintenance;
+    private Long communityId; // solo el id de la comunidad
+}
---
+++ core/src/main/java/com/dam/commune/invoice/InvoiceRepository.java
@@ -0,0 +1,11 @@
+package com.dam.commune.invoice;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
+
+    List<Invoice> findByCommunityId(Long communityId);
+    
+}
