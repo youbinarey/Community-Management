7c8975d6423d113c107a05eae467dac36d2eeddf
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-31 00:28:40 +0200

    feat: implement InvoicesComponent with community ID handling and invoice loading

--- frontend/src/app/pages/invoices/invoices.component.ts
+++ frontend/src/app/pages/invoices/invoices.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Invoice } from '../../models/Invoice';
+import { InvoiceService } from '../../service/invoice-service.service';
+import { CommonModule, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-invoices',
-  imports: [],
   templateUrl: './invoices.component.html',
-  styleUrl: './invoices.component.scss'
+  styleUrls: ['./invoices.component.scss'],
+  imports: [NgFor, CommonModule],
 })
-export class InvoicesComponent {
+export class InvoicesComponent implements OnInit {
 
+  communityId!: number;
+  invoices: Invoice[] = [];
+
+  constructor(
+    private route: ActivatedRoute,
+    private invoiceService: InvoiceService,
+  ) {}
+
+  ngOnInit() {
+    this.communityId = Number(this.route.snapshot.paramMap.get('communityId'));
+    this.loadInvoices();
+  }
+
+  loadInvoices() {
+    this.invoiceService.getInvoicesByCommunity(this.communityId).subscribe(data => {
+      this.invoices = data;
+    });
+  }
+
+ // TODO DESCARGAR PDF
 }
