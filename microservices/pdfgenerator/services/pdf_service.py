d40f4fc083cfc92ee66c2df3fdc08687cc653555
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-30 23:44:42 +0200

    feat: refactor PDF generation to use dynamic template paths and include CSS for owner receipts

--- microservices/pdfgenerator/services/pdf_service.py
+++ microservices/pdfgenerator/services/pdf_service.py
@@ -1,11 +1,16 @@
 import io
-from weasyprint import HTML
+from pathlib import Path
+from weasyprint import CSS, HTML
 from utils.helpers import format_spanish_date
 
+# Calcula el directorio base (donde está este script)
+BASE_DIR = Path(__file__).resolve().parent.parent  # Sube dos niveles si este código está en /services
+
 def generate_receipt_pdf(invoice):
-    
     fecha_formateada = format_spanish_date(invoice.date)
-    with open("templates/template.html", encoding="utf-8") as f:
+    template_path = BASE_DIR / "templates" / "template.html"
+
+    with open(template_path, encoding="utf-8") as f:
         html_content = f.read().format(
             address=invoice.address,
             date=fecha_formateada,
@@ -15,8 +20,28 @@ def generate_receipt_pdf(invoice):
             elevator=invoice.elevator,
             maintenance=invoice.maintenance,
         )
-        
     pdf_file = io.BytesIO()
     HTML(string=html_content).write_pdf(pdf_file)
+    pdf_file.seek(0)
+    return pdf_file
+
+def generate_owner_receipt_pdf(invoice):
+    template_path = BASE_DIR / "templates" / "owner_template.html"
+    css_path = BASE_DIR / "templates" / "css" / "owner_template_styles.css"
+
+    with open(template_path, encoding="utf-8") as f:
+        html_content = f.read().format(
+            id=invoice.id,
+            date=invoice.date,
+            invoiceId=invoice.invoiceId,
+            ownerName=invoice.ownerName,
+            ownerSurname=invoice.ownerSurname,
+            ownerBankAccount=invoice.ownerBankAccount,
+            coefficient=f"{float(invoice.coefficient):.2f}",
+            amount=f"{float(invoice.amount):.2f}",
+            communityName=invoice.communityName
+        )
+    pdf_file = io.BytesIO()
+    HTML(string=html_content).write_pdf(pdf_file, stylesheets=[CSS(str(css_path))])
     pdf_file.seek(0)
     return pdf_file
