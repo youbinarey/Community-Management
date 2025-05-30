56b218c6585182c9e773fc15c7de13d524d6c4c9
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-30 23:45:35 +0200

    feat: implement endpoints for generating community and owner receipts

--- microservices/pdfgenerator/app.py
+++ microservices/pdfgenerator/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, send_file
 from models.invoice import Invoice
+from models.invoice_owner import OwnerInvoice
+from services.pdf_service import generate_owner_receipt_pdf
 from services.pdf_service import generate_receipt_pdf
 
 app = Flask(__name__)
 
-@app.route('/generate-pdf', methods=['POST'])
+@app.route('/generate-community-receipt', methods=['POST'])
 def generate_pdf_endpoint():
     data = request.json
     invoice = Invoice(**data)
@@ -13,6 +15,18 @@ def generate_pdf_endpoint():
         pdf_file,
         as_attachment=True,
         download_name='recibo.pdf',
+        mimetype='application/pdf'
+    )
+    
+@app.route('/generate-owner-receipt', methods=['POST'])
+def generate_owner_receipt():
+    data = request.json
+    invoice = OwnerInvoice(**data)
+    pdf_file = generate_owner_receipt_pdf(invoice)
+    return send_file(
+        pdf_file,
+        as_attachment=True,
+        download_name='recibo_propietario.pdf',
         mimetype='application/pdf'
     )
 
