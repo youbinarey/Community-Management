300b879ef1e4b824a79c99074a45ecdaf95fe5ab
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-31 00:21:14 +0200

    feat: add CrossOrigin annotation to Invoice and OwnerInvoice controllers

--- core/src/main/java/com/dam/commune/invoice/InvoicePdfController.java
+++ core/src/main/java/com/dam/commune/invoice/InvoicePdfController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/invoices")
+@CrossOrigin(origins = "http://localhost:4200")
+
 public class InvoicePdfController {
 
     private final InvoiceService invoiceService;
