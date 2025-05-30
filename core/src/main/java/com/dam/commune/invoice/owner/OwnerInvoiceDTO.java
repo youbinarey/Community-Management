81dd39543da94f67f314bf9aa30240453a91f7df
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-30 23:42:11 +0200

    feat: create OwnerInvoiceDTO class for owner invoice data transfer

---
+++ core/src/main/java/com/dam/commune/invoice/owner/OwnerInvoiceDTO.java
@@ -0,0 +1,25 @@
+package com.dam.commune.invoice.owner;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OwnerInvoiceDTO {
+    private Long id;
+    private String date;
+    private Double amount;
+    private Long invoiceId;
+    private String ownerName;
+    private String ownerSurname;
+    private String ownerBankAccount;
+    // private List<Properties> properties;
+    private Double coefficient;
+    private String communityName;
+}
