0a0dff8ba458832aa9c1c6d1dbaea24548c0bcd7
youbinarey <yeray.oubina.rey@gmail.com>
2025-05-31 02:11:34 +0200

    feat: refactor InvoiceService to streamline invoice retrieval and download methods

--- frontend/src/app/service/invoice-service.service.ts
+++ frontend/src/app/service/invoice-service.service.ts
@@ -1,23 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Invoice } from '../models/Invoice';
+import { InvoiceOwner } from '../models/InvoiceOwner';
+import { InvoiceCommunity } from '../models/InvoiceCommunity copy';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InvoiceService {
 
-  private apiUrl = 'http://localhost:8080/api/commune/invoices';
+  private apiUrl = 'http://localhost:8080/api/commune';
 
   constructor(private http: HttpClient) { }
 
-  getInvoicesByCommunity(communityId: number): Observable<Invoice[]> {
-    return this.http.get<Invoice[]>(`${this.apiUrl}/dto/community/${communityId}`);
+  getInvoicesByCommunity(communityId: number): Observable<InvoiceCommunity[]> {
+    return this.http.get<InvoiceCommunity[]>(`${this.apiUrl}/invoices/dto/community/${communityId}`);
   }
 
-  downloadInvoice(invoiceId: number): Observable<Blob> {
-  return this.http.get(`${this.apiUrl}/${invoiceId}/pdf`, { responseType: 'blob' });
-}
+  downloadCommunityInvoice(invoiceId: number): Observable<Blob> {
+  return this.http.get(`${this.apiUrl}/invoices/${invoiceId}/pdf`, { responseType: 'blob' });
+  }
+
+  getInvoicesByOwner(ownerId: number): Observable<InvoiceOwner[]> {
+    return this.http.get<InvoiceOwner[]>(`${this.apiUrl}/owner-invoices/owner/${ownerId}`);
+  }
+  
+  downloadOwnerInvoice(invoiceId: number): Observable<Blob> {
+  return this.http.get(`${this.apiUrl}/invoices/owner-invoice/${invoiceId}/pdf`, { responseType: 'blob' });
+  }
+ 
+
 
 }
