d43b0359712e2db2baa2292075ed70910d30312c
youbinarey <yeray.oubina.rey@gmail.com>
2025-06-22 13:50:32 +0200

    docs: add docs for Community interface

--- frontend/src/app/models/Community.ts
+++ frontend/src/app/models/Community.ts
@@ -2,6 +2,21 @@
 import { Property } from "./Property";
 
 
+/**
+ * Represents a community entity with its basic information and optional features.
+ *
+ * @property id - Unique identifier for the community.
+ * @property address - Physical address of the community.
+ * @property postalCode - Postal code of the community's location.
+ * @property elevator - Indicates if the community has an elevator (optional).
+ * @property numFlats - Number of flats in the community (optional).
+ * @property numparkings - Number of parking spaces in the community (optional).
+ * @property numStorageRooms - Number of storage rooms in the community (optional).
+ * @property reducedMobilityAccess - Indicates if the community has access for reduced mobility (optional).
+ * @property bankAccountNumber - Bank account number associated with the community (optional).
+ * @property properties - List of properties belonging to the community (optional).
+ * @property propertiesCount - Total number of properties in the community (optional).
+ */
 export interface Community {
   id: number;
   address: string;
