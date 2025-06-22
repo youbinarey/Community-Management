23064b8edc9b00fb4d523dfc9b45d71bfe5a6b46
youbinarey <yeray.oubina.rey@gmail.com>
2025-06-22 13:51:51 +0200

    docs: add docs for Owner interface

--- frontend/src/app/models/Owner.ts
+++ frontend/src/app/models/Owner.ts
@@ -1,3 +1,17 @@
+/**
+ * Represents an owner entity with personal and contact information.
+ *
+ * @property isEditMode - Indicates if the owner is currently in edit mode.
+ * @property id - Unique identifier for the owner.
+ * @property dni - National identification number of the owner.
+ * @property name - First name of the owner.
+ * @property surname - Last name of the owner.
+ * @property email - Email address of the owner.
+ * @property phone - Phone number of the owner.
+ * @property birthDate - Birth date of the owner.
+ * @property bankAccountNumber - Bank account number associated with the owner.
+ * @property propertiesCount - Number of properties owned.
+ */
 export interface Owner {
 isEditMode: any;
   id: number;
